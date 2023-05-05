52c50e9c93f1f272258f8dc4e551ac82b8e4d81a
Beachnewt <beachnate10@gmail.com>
2023-05-04 23:14:11 -0500

    Changed Scripts
    WARNING: Chnaged Scene_NN so hopefully it doesn't break.
    
    - Removed some debug lines from Mole.cs.
    - Made it so that the gun rotates to where you're going to shoot

--- Mole Mania/Assets/__Scripts/TargetDetector.cs
+++ Mole Mania/Assets/__Scripts/TargetDetector.cs
@@ -1,25 +1,58 @@
+/*****
+ * Created by: Nathan Nguyen
+ * Created on: ---
+ * 
+ * Last edited by: Nathan Nguyen
+ * Last edited on: 5/4/2023
+ * 
+ * Description: Raycasts and find a world position to shoot the bullet. Also rotates the gun to world position mouse is at.
+ *****/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetDetector : MonoBehaviour
 {
-    public Vector3 startingPosition; 
+    private Vector3 bulletSpawnPosition; 
 
     [Header("Inscribed")]
     public LayerMask layersToHit;
     public GameObject bullet;
+    public float speed;
 
     [Header("Dynamic")]
     public Vector3 screenPosition;
     public Vector3 worldPosition;
+    public Vector3 screenPositionRotate;
+    public Vector3 worldPositionRotate;
 
-    void Awake() {
-        startingPosition = transform.position;
+    private Transform gunParent;
+
+    void Start() {
+        speed = 5.0f;
+        gunParent = transform.parent;
     }
 
     void Update() {
+
+        screenPositionRotate = Input.mousePosition;
+
+        Ray rotateRay = Camera.main.ScreenPointToRay(screenPositionRotate);
+
+        if (Physics.Raycast(rotateRay, out RaycastHit hitDataRotate, 100, layersToHit)) {
+            worldPositionRotate = hitDataRotate.point;
+        }
+
+        Vector3 newDirection = (worldPositionRotate - gunParent.position).normalized;
+        Debug.DrawRay(gunParent.position, newDirection, Color.red);
+
+        gunParent.rotation = Quaternion.LookRotation(newDirection, Vector3.up);
+
+        // Shoots bullet towards raycast position
         if (Input.GetMouseButtonDown(0)) {
+            bulletSpawnPosition = transform.GetChild(1).position;
+
             screenPosition = Input.mousePosition;
 
             Ray ray = Camera.main.ScreenPointToRay(screenPosition);
@@ -28,7 +61,7 @@ public class TargetDetector : MonoBehaviour
                 worldPosition = hitData.point;
             }
 
-            GameObject go = Instantiate(bullet, startingPosition, bullet.transform.rotation).gameObject;
+            GameObject go = Instantiate(bullet, bulletSpawnPosition, bullet.transform.rotation).gameObject;
             go.GetComponent<FireProjectile>().targetPosition = worldPosition;
         }
     }
